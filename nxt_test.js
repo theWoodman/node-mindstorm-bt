b734a68394b0262ec46e95379b94c7618ec75353
David Fotel <davsebamse@gmail.com>
2014-12-23 14:09:36 +0100

    Updated example to events
    Updated example to show use of examples.

--- nxt_test.js
+++ nxt_test.js
@@ -1,13 +1,24 @@
 var Nxt = require("./nxt").Nxt;
+var nxt = new Nxt("COM4");
 
-function play_sound_test() {
-	var nxt = new Nxt("/dev/rfcomm0");
-	//nxt.play_tone(440, 1000);
-	//nxt.set_output_state(nxt.MOTOR_A, 50, nxt.MOTORON, nxt.REGULATION_MODE_IDLE, 0, nxt.MOTOR_RUN_STATE_RUNNING, 360);
-	nxt.debug = true;
-	nxt.set_input_state(nxt.INPUT_PORT_2, nxt.SWITCH, nxt.BOOLEANMODE);
-	nxt.get_input_values(nxt.INPUT_PORT_2);
-	setTimeout(nxt.close_connection(nxt.sp),10000);
-}
+nxt.on('getinputvalue', function(data) {
+    if(data[3] == nxt.INPUT_PORT_1){
+        var adc = (data[11] << 8) | data[10];
+        console.log('Touch ADC: ' + adc);
+    }
+});
 
-play_sound_test();
+nxt.sp.on("open", function () {
+    // Init
+    console.log("Robot connected")
+    nxt.play_tone(440, 500);
+
+    // Set up hardware
+    nxt.set_input_state(nxt.INPUT_PORT_1, nxt.SWITCH, nxt.BOOLEANMODE);
+    console.log("Hardware set up");
+
+    // Start interval
+    setInterval(function(){
+        nxt.get_input_values(nxt.INPUT_PORT_1);
+    }, 500);
+});
